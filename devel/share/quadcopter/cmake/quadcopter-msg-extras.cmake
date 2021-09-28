2fc02ba43046629c649d2f5dd4d784200fb1c574
Sashank MNS <sashankmodali@gmail.com>
2021-09-28 00:07:48 -0400

    Add files via upload

---
+++ devel/share/quadcopter/cmake/quadcopter-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(quadcopter_MESSAGE_FILES "/home/sashankmns/catkin_ws/src/quadcopter/msg/TargetInfo.msg;/home/sashankmns/catkin_ws/src/quadcopter/msg/Contour.msg;/home/sashankmns/catkin_ws/src/quadcopter/msg/kalman.msg")
+set(quadcopter_SERVICE_FILES "")
---
+++ devel/share/quadcopter/cmake/quadcopter-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(quadcopter_MSG_INCLUDE_DIRS "/home/sashankmns/catkin_ws/src/quadcopter/msg")
+set(quadcopter_MSG_DEPENDENCIES geometry_msgs;sensor_msgs;std_msgs)
